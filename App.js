bccbc08096b486afad019fddd3e31b70fb1d6fad
Mateus <mateusbm00@outlook.com>
2025-01-28 17:27:11 -0300

    Ajustes do App.js

--- App.js
+++ App.js
@@ -1,13 +1,7 @@
 import * as React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import ServicosScreen from './screens/Servicos';
 import AgendamentoScreen from './screens/agendamento';
-import { FontAwesome5, AntDesign } from '@expo/vector-icons';
 
-
-const Tab = createBottomTabNavigator()
 
 function App(){
   return(
@@ -17,39 +11,6 @@ function App(){
       </View>
       <AgendamentoScreen/>
     </View>
-    
-    /*<NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#70a1ff', // Cor de fundo do header
-          },
-          headerTintColor: '#FFF', // Cor do título no header
-          tabBarActiveTintColor: '#70a1ff', // Cor da tab selecionada
-          tabBarInactiveTintColor: '#777', // Cor da tab não selecionada
-        }}
-      >
-        <Tab.Screen 
-          name='Serviços' 
-          component={ServicosScreen}
-          options={{
-            tabBarIcon: ({color, size}) =>(
-              <FontAwesome5 name="toolbox" size={size} color={color} />
-            )
-          }}
-          />
-
-        <Tab.Screen 
-          name='Agendamento' 
-          component={AgendamentoScreen}
-          options={{
-            tabBarIcon: ({color, size}) =>(
-              <FontAwesome5 name="calendar" size={size} color={color} />
-            )
-          }}
-          />
-      </Tab.Navigator>
-    </NavigationContainer>*/
   )
 }
 
